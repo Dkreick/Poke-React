efbdb99fbbc1fc8f68728671b74cbc32938d2d40
Dkreick <damian.kreick@gmail.com>
2020-07-29 14:28:05 -0300

    Adds header

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.scss';
+import './app.scss';
+import Header from './header/header'
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/app/header/header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  toolbar: {
+    backgroundColor:"red",
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h6" className={classes.title}>
+            Poke React
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
