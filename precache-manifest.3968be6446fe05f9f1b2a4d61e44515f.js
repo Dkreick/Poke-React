d0b60d85792a892cb6bdd3ae95d4e03ed09de669
Dkreick <Dkreick@users.noreply.github.com>
2020-08-16 04:11:02 +0000

    deploy: 1656372d5ac7bcf482a35e9e5bbc1fe7d88d34c6

--- precache-manifest.3968be6446fe05f9f1b2a4d61e44515f.js
+++ precache-manifest.3968be6446fe05f9f1b2a4d61e44515f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "110ffbe418e5433789c1fb54fe58b187",
+    "revision": "14102c468926ac8eb70692b158bcbe69",
     "url": "/poke-react/index.html"
   },
   {
-    "revision": "449d848e11478212e69d",
+    "revision": "3699afd39f89e08eacaf",
     "url": "/poke-react/static/css/main.de5618a2.chunk.css"
   },
   {
-    "revision": "1d4de3cd0bd60d1ca4dd",
-    "url": "/poke-react/static/js/2.574d518c.chunk.js"
+    "revision": "e1d60fecd0f8b553d945",
+    "url": "/poke-react/static/js/2.016a2a51.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/poke-react/static/js/2.574d518c.chunk.js.LICENSE.txt"
+    "url": "/poke-react/static/js/2.016a2a51.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "449d848e11478212e69d",
-    "url": "/poke-react/static/js/main.c7dccaee.chunk.js"
+    "revision": "3699afd39f89e08eacaf",
+    "url": "/poke-react/static/js/main.b2fa9bec.chunk.js"
   },
   {
     "revision": "f7f9ff212ec75071f535",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/poke-react/precache-manifest.0bac06c469977e631250e42b7a128ea7.js"
+  "/poke-react/precache-manifest.3968be6446fe05f9f1b2a4d61e44515f.js"
 );
 
 self.addEventListener('message', (event) => {
